622c522f207efccb020c9b53b0cf93f160949d59
MekariKana <20190101km@gmail.com>
2025-04-11 13:19:18 +0900

    add hamburger menu

--- includes/headerFooterManeger.js
+++ includes/headerFooterManeger.js
@@ -4,8 +4,10 @@
 //実行環境
 const basePath = "/KamekaBlog/";
 
+//ベースurl
 const baseUrl = `${window.location.origin}${basePath}`;
 
+//ヘッダーの挿入
 class SpecialHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
@@ -16,7 +18,12 @@ class SpecialHeader extends HTMLElement {
                 </div>
             </header>
             <nav class="w_inner">
-                <ul>
+                <div class="hamburger" id="hamburger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <ul class="nav_list" id="nav_list"> 
                     <li><a class="nav-link" href="${baseUrl}index.html">Home</a></li>
                     <li><a class="nav-link" href="${baseUrl}portfolio.html">Portfolio</a></li>
                     <li><a class="nav-link" href="${baseUrl}study.html">Study</a></li>
@@ -28,6 +35,7 @@ class SpecialHeader extends HTMLElement {
     }
 }
 
+//フッターの挿入
 class SpecialFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
@@ -42,6 +50,14 @@ class SpecialFooter extends HTMLElement {
 
 customElements.define("special-header", SpecialHeader)
 customElements.define("special-footer", SpecialFooter)
+
+//ハンバーガーメニューの変数
+const hamburger = document.getElementById('hamburger');
+const navList = document.getElementById('nav_list');
+//ハンバーガーメニューの追加
+hamburger.addEventListener('click', () => {
+    navList.classList.toggle('active');
+});
 
 //current pageのcssを付与
 document.querySelectorAll('.nav-link').forEach(link => {
@@ -60,7 +76,6 @@ document.querySelectorAll('.nav-link').forEach(link => {
 
 
 // `study_contents` 内のページなら、study.html のリンクを探して `current` を付ける
-
 if (window.location.pathname.startsWith(`${basePath}study_contents/`)) {
     document.querySelectorAll('.nav-link').forEach(nav => {
         if (nav.href.endsWith('study.html')) {
